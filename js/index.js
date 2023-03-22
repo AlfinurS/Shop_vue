7915dab18ef479984c7c1defcffcab9205209b34
AlfinurS <aaalfinur87@gmail.com>
2023-03-22 18:24:33 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -5,10 +5,12 @@ const app = Vue.createApp({
         return {
             catalogUrl: `/catalogData.json`,
             cartUrl: `/getBasket.json`,
+            deleteCartUrl: `/deleteFromBasket.json`,
             products: [],
             imgCatalog: 'https://placehold.it/200x150',
             userSearch: '',
             cart: [],
+            isShowCart: false,
         }
     },
     methods: {
@@ -23,17 +25,40 @@ const app = Vue.createApp({
         addProduct(product){
             const index = this.products.findIndex((item) => item.id_product === product.id_product);
             let productFind = this.cart.find(product => product.id_product === this.products[index].id_product);
-            console.log(productFind);
             if (productFind) {
-                productFind.quantity++;
+                ++productFind.quantity;
                 //this.showCart(productFind)
+            } else {
+                const cartProduct = JSON.parse(JSON.stringify(product))
+                cartProduct.quantity = 1;
+                this.cart.push(cartProduct);
+            console.log(this.cart);
             }
         },
 
-        showCart(){
-            console.log("ok");
+       showCart(){
+            
+            this.isShowCart = !this.isShowCart;
+        }, 
+        
+        deleteProduct(product){
+            this.getJson(`${API + this.deleteCartUrl}`)
+            .then(data => {
+                 if(data.result===1){
+                    const index = this.products.findIndex((item) => item.id_product === product.id_product);
+                    let productFind = this.cart.find(product => product.id_product === this.products[index].id_product);
+                    if(productFind.quantity > 1){
+                        productFind.quantity--;
+                } else {
+                    this.cart.splice(this.cart.indexOf(productFind), 1);
+                }
+            }
+        })
         }
     },
+
+    
+
     mounted(){
         this.getJson(`${API + this.catalogUrl}`)
             .then(data => {
